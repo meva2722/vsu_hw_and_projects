684f5bd12de53d8689aa609deb27b5f329813bbe
Marcus Evans <67246215+meva2722@users.noreply.github.com>
2022-11-04 14:36:01 -0400

    Add files via upload
    lab 04

---
+++ lab04/lab04/main.swift
@@ -0,0 +1,155 @@
+/*
+* Program: lab04.swift
+*
+* Author: Marcus J Evans
+*
+* Date: 09/22/22
+* Course: CSCI495
+*
+* Program Description:
+*
+* WRITE PROGRAM DESCRIPTION
+*
+*/
+
+
+
+import Foundation
+//import UIKit
+
+//print("enter first side: ")
+//if let input1 = readLine(){
+//    if let num1 = Int(input1){
+//        print("user entered: \(num1)")
+//    } else{
+//        print("Please enter an integer!")
+//    }
+//}
+//
+//print("enter second side: ")
+//if let input2 = readLine(){
+//    if let num2 = Int(input2){
+//        print("user entered: \(num2) ")
+//    } else{
+//        print("Please enter an integer!")
+//    }
+//}
+//
+//print("enter third side: ")
+//if let input3 = readLine(){
+//    if let num3 = Int(input3){
+//        print("user entered: \(num3) ")
+//    } else{
+//        print("Please enter an integer!")
+//    }
+//}
+//
+//
+//
+
+
+
+
+
+//
+//func equilateral(Num1:Int , Num2:Int, Num3:Int){
+//    if (num1 == num2 && num1 == num3) {
+//        print("The triangle is equilateral. ")
+//    } else{
+//        print("The triangle is not equilateral. ")
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//if let input1 = readLine(){
+//    if let num1 = Int(input1){
+//        print("user entered: \(num1)")
+//    } else{
+//        print("Please enter an integer!")
+//    }
+//} else{
+//    print("No input recieved")
+//}
+//
+//print("enter second side: ")
+//if let input2 = readLine(){
+//    if let num2 = Int(input2){
+//        print("user entered: \(num2) ")
+//    } else{
+//        print("Please enter an integer!")
+//    }
+//} else{
+//    print("No input recieved")
+//}
+//
+//print("enter third side: ")
+//if let input3 = readLine(){
+//    if let num3 = Int(input3){
+//        print("user entered: \(num3) ")
+//    } else{
+//        print("Please enter an integer!")
+//    }
+//} else{
+//    print("No input recieved")
+//}
+//
+//
+//
+//equilateral(Num1: num1, Num2: num2, Num3: num3)
+//
+//
+
+
+
+class Triangle {
+
+
+    print("enter first side: ")
+    if let input1 = readLine(){
+        if let num1 = Int(input1){
+            print("user entered: \(num1)")
+        } else{
+            print("Please enter an integer!")
+        }
+    }
+
+    print("enter second side: ")
+    if let input2 = readLine(){
+        if let num2 = Int(input2){
+            print("user entered: \(num2) ")
+        } else{
+            print("Please enter an integer!")
+        }
+    }
+
+    print("enter third side: ")
+    if let input3 = readLine(){
+        if let num3 = Int(input3){
+            print("user entered: \(num3) ")
+        } else{
+            print("Please enter an integer!")
+        }
+    }
+
+
+    func equilateral(Num1:Int , Num2:Int, Num3:Int){
+        if (num1 == num2 && num1 == num3) {
+            print("The triangle is equilateral. ")
+        } else{
+            print("The triangle is not equilateral. ")
+        }
+    }
+
+
+
+
+//}
+
+equilateral(Num1: num1, Num2: num2, Num3: num3)
+
